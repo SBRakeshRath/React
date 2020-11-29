b514b9ffa60117ba464ee60c856262d31df842bb
SBRakeshRath <75027068+SBRakeshRath@users.noreply.github.com>
2020-11-29 19:13:41 +0530

    First Commit

---
+++ Learning codes/index.js files/FIrstChallenge.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+ReactDOM.render(
+        <>
+                <h1>S B Rakesh Rath Netflix Pick</h1>
+                <p>List of 5 Best Series</p>
+
+                <ol>
+                        <li>Dark</li>
+                        <li>Peaky Blinders</li>
+                        <li>Iron Fist</li>
+                        <li>The 100</li>
+                        <li>I don't know</li>
+                </ol>
+        </>
+        ,
+        document.getElementById('root')
+);
+
